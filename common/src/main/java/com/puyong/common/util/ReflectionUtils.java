658e93fecd64bf7a64ca0f24e00266c1639bebfd
puyong <ppp1296286317@163.com>
2023-08-30 23:22:39 +0800

    1.0.0 feature 反射获取私有属性字段

--- common/src/main/java/com/puyong/common/util/ReflectionUtils.java
+++ common/src/main/java/com/puyong/common/util/ReflectionUtils.java
@@ -1,6 +1,17 @@
 package com.puyong.common.util;
 
+import java.lang.reflect.Field;
+import java.util.Objects;
+
 public class ReflectionUtils {
 
-
+    public static Object getFieldVal(Object obj, String fieldName) throws IllegalAccessException {
+        for (Field field : obj.getClass().getFields()) {
+            if (Objects.equals(field.getName(), fieldName)) {
+                field.setAccessible(true);
+                return field.get(obj);
+            }
+        }
+        return null;
+    }
 }
